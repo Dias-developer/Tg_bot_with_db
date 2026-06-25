19603e0982c6799c3872f67323e93ba90efdc3e2
Dias <kasenov_06@internet.ru>
2026-06-25 12:09:14 +0500

    changed and fixed bugs

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ from os import getenv
 import logging
 
 from handlers import router
-
+from db import create_table
 load_dotenv()
 token = getenv("TOKEN")
 dp = Dispatcher()
 
 async def main():
+
+    create_table()
+
     dp.include_router(router)
     bot = Bot(token=token)
     await dp.start_polling(bot)
