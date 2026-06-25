63a2945d12f85c29068fadc67ed10ee932effe2f
Dias <kasenov_06@internet.ru>
2026-06-25 11:34:42 +0500

    connected SQLite3 and added new functions

---
+++ db.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+def create_table():
+    db = sqlite3.connect('database/clients.db')
+    c = db.cursor()
+
+    c.execute(''' CREATE TABLE IF NOT EXISTS clients (
+                  id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  username TEXT,
+                  phone_number TEXT,
+                  email TEXT,
+                  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)''')
+    db.commit()
+    db.close()
+
+def add_clients(username, phone_num, email):
+    db = sqlite3.connect('database/clients.db')
+    c = db.cursor()
+
+    c.execute('INSERT INTO clients (username, phone_number, email) VALUES (?, ?, ?)', (username, phone_num, email))
+
+    db.commit()
+
+    db.close()
+
+def show_clients():
+    db = sqlite3.connect('database/clients.db')
+    c = db.cursor()
+
+    c.execute('SELECT * FROM clients')
+    clients = c.fetchall()
+
+    db.close()
+
+    return clients
