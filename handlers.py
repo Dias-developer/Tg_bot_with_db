d74567939b98995c28c95067dabdc152459a6d9f
Dias <kasenov_06@internet.ru>
2026-06-26 18:11:21 +0500

    added FSM

--- handlers.py
+++ handlers.py
@@ -1,8 +1,10 @@
 from aiogram import Router, F
+from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, KeyboardButton, ReplyKeyboardMarkup
 from aiogram.filters import CommandStart
-import asyncio
 
+
+from state.state import AddClient, DeleteClient
 from db import add_clients, show_clients, delete_client_by_id
 router = Router()
 
@@ -22,49 +24,78 @@ async def cmd_start(message: Message):
     user = message.from_user
     await message.answer(
         f"Привет, {user.first_name}! 👋\n"
-        "Я бот для учета клиентов.",
+        "Я бот для учета клиентов.\n"
+        "Если хотите взаимодействовать с ботом, то воспользуйтесь меню!",
         reply_markup=crud_inline_keyboard(),
     )
+
 # Add clients
 @router.message(F.text == "Добавить")
-async def add_client_menu(message: Message):
-    await message.answer(
-        "Введите данные клиента через запятую:\n\n"
-        "Имя,Телефон,Email"
+async def add_client_menu(message: Message, state: FSMContext):
+    await state.set_state(AddClient.waiting_for_username)
+    await message.answer('Введите имя клиента:')
+
+@router.message(AddClient.waiting_for_username)
+async def add_client_username(message: Message, state: FSMContext):
+   username = message.text.strip()
+
+   if len(username) > 20:
+       await message.answer('Слишком длинное имя!')
+       return
+
+   if not username.isalpha():
+       await message.answer("Имя должно содержать только буквы!")
+       return
+
+   await state.update_data(username=username)
+
+   await state.set_state(AddClient.waiting_for_phone)
+
+   await message.answer('Введите номер телефона:')
+
+
+@router.message(AddClient.waiting_for_phone)
+async def add_client_phone(message: Message, state: FSMContext):
+    phone = message.text.strip()
+
+    if not phone.replace("+", "").isdigit():
+        await message.answer(
+            "Телефон должен содержать только цифры и +"
+        )
+        return
+
+    if len(phone) < 11:
+        await message.answer('Номер телефона должно содержать 10 цифр!')
+        return
+
+    await state.update_data(phone=phone)
+
+    await state.set_state(AddClient.waiting_for_email)
+
+    await message.answer("Введите email:")
+
+@router.message(AddClient.waiting_for_email)
+async def add_client_email(message: Message, state: FSMContext):
+    email = message.text.strip()
+
+    if '@' not in email or '.' not in email:
+        await message.answer(
+            "Введите корректный email."
+        )
+        return
+
+    await state.update_data(email=email)
+
+    data = await state.get_data()
+
+    add_clients(
+        data["username"],
+        data["phone"],
+        data["email"]
     )
 
-@router.message(lambda msg: ',' in msg.text)
-async def add_client_handler(message: Message):
-    try:
-        username, phone, email = [
-            item.strip()
-            for item in message.text.split(",")
-        ]
-
-        if len(username) > 20:
-            await message.answer(
-                "Имя должно быть не длиннее 20 символов."
-            )
-            return
-
-        if not username.isalpha():
-            await message.answer(
-                "Имя должно содержать только буквы."
-            )
-            return
-
-        add_clients(username, phone, email)
-        await asyncio.sleep(1)
-        await message.answer(
-            "✅ Клиент успешно добавлен."
-        )
-    except ValueError:
-        await message.answer(
-            "Неверный формат.\n"
-            "Пример:\n"
-            "Иван,77771234567,ivan@gmail.com"
-        )
-
+    await state.clear()
+    await message.answer("✅ Клиент успешно добавлен.")
 # Show clients
 @router.message(F.text == "Показать")
 async def show_client_menu(message: Message):
@@ -88,27 +119,27 @@ async def show_client_menu(message: Message):
 
 # delete clients
 @router.message(F.text == "Удалить")
-async def delete_clients_menu(message: Message):
-    await message.answer('Если хотите удалить клиента по ID, то воспользуйтесь командой /delete -> номер ID <-')
+async def delete_clients_menu(message: Message, state: FSMContext):
+    await state.set_state(DeleteClient.waiting_for_id)
 
-@router.message(F.text.startswith("/delete"))
-async def delete_client_handler(message: Message):
-    try:
-        client_id = int(message.text.split()[1])
+    await message.answer(
+        "Введите ID клиента:"
+    )
 
-        delete_client_by_id(client_id)
+@router.message(DeleteClient.waiting_for_id)
+async def delete_client_handler(message: Message, state: FSMContext):
+    client_id = message.text.strip()
 
-        await message.answer(
-            f"✅ Клиент с ID {client_id} удалён."
-        )
+    if not client_id.isdigit():
+        await message.answer("ID должен быть числом!")
+        return
 
-    except IndexError:
-        await message.answer(
-            "Укажите ID.\n"
-            "Пример: /delete 3"
-        )
+    client_id = int(client_id)
 
-    except ValueError:
-        await message.answer(
-            "ID должен быть числом."
-        )
+    delete_client_by_id(client_id)
+
+    await state.clear()
+
+    await message.answer(
+        f"✅ Клиент с ID {client_id} удалён."
+    )
---
+++ state/state.py
@@ -0,0 +1,11 @@
+from aiogram.fsm.state import State, StatesGroup
+
+
+class AddClient(StatesGroup):
+    waiting_for_username = State()
+    waiting_for_phone = State()
+    waiting_for_email = State()
+
+
+class DeleteClient(StatesGroup):
+    waiting_for_id = State()
